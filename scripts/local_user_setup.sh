37e6d50eb0673e1c89de18d1772a51a2a3e09559
Luca Weiss <luca@z3ntu.xyz>
2018-09-10 19:44:02 +0200

    Add script to simplify running as non-root user

---
+++ scripts/local_user_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+# Permissions to hidraw files
+sudo chmod g+rw /dev/hidraw*
+sudo chgrp $(id -g) /dev/hidraw*
+
+user=$(id -nu)
+# Permission to dbus
+sudo sed -i 's|user="root"|user="'$user'"|' /usr/share/dbus-1/system.d/io.github.openrazer1.conf
+
