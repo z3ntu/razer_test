06139a58685e7872ed2caf615e8d870bcdec4656
Luca Weiss <luca@z3ntu.xyz>
2019-03-31 16:41:53 +0200

    Improve razerreport.{cpp,h}

--- src/razerreport.h
+++ src/razerreport.h
@@ -24,43 +24,45 @@
 
 using namespace razer_test;
 
-union transaction_id_union {
-    unsigned char id;
-    struct transaction_parts {
-        unsigned char device : 3;
-        unsigned char id : 5;
-    } parts;
+struct razer_report_struct {
+    uchar status;
+
+    union {
+        uchar transaction_id;
+        struct {
+            uchar device : 3;
+            uchar id : 5;
+        } transaction_id_parts;
+    };
+
+    ushort remaining_packets;
+    uchar protocol_type;
+    uchar data_size;
+    uchar command_class;
+
+    union {
+        uchar command_id;
+        struct {
+            uchar direction : 1;
+            uchar id : 7;
+        } command_id_parts;
+    };
+
+    uchar arguments[80];
+    uchar crc;
+    uchar reserved;
 };
+using razer_report = razer_report_struct;
 
-union command_id_union {
-    unsigned char id;
-    struct command_id_parts {
-        unsigned char direction : 1;
-        unsigned char id : 7;
-    } parts;
-};
 
-typedef struct razer_report {
-    unsigned char status;
-    union transaction_id_union transaction_id; /* */
-    unsigned short remaining_packets; /* Big Endian */
-    unsigned char protocol_type; /*0x0*/
-    unsigned char data_size;
-    unsigned char command_class;
-    union command_id_union command_id;
-    unsigned char arguments[80];
-    unsigned char crc;/*xor'ed bytes of report*/
-    unsigned char reserved; /*0x0*/
-} razer_report;
+uchar razer_calculate_crc(razer_report *report);
+razer_report get_razer_report(uchar command_class, uchar command_id, uchar data_size);
 
-unsigned char razer_calculate_crc(razer_report *report);
-razer_report get_razer_report(unsigned char command_class, unsigned char command_id, unsigned char data_size);
-
-razer_report razer_chroma_standard_set_led_brightness(RazerVarstore variable_storage, RazerLedId led_id, unsigned char brightness);
+razer_report razer_chroma_standard_set_led_brightness(RazerVarstore variable_storage, RazerLedId led_id, uchar brightness);
 razer_report razer_chroma_standard_get_led_brightness(RazerVarstore variable_storage, RazerLedId led_id);
 razer_report razer_chroma_standard_set_led_effect(RazerVarstore variable_storage, RazerLedId led_id, RazerClassicEffectId led_effect);
 razer_report razer_chroma_standard_get_led_effect(RazerVarstore variable_storage, RazerLedId led_id);
-razer_report razer_chroma_standard_set_led_rgb(RazerVarstore variable_storage, RazerLedId led_id, unsigned char red, unsigned char green, unsigned char blue);
+razer_report razer_chroma_standard_set_led_rgb(RazerVarstore variable_storage, RazerLedId led_id, uchar red, uchar green, uchar blue);
 razer_report razer_chroma_standard_get_led_rgb(RazerVarstore variable_storage, RazerLedId led_id);
 razer_report razer_chroma_standard_set_led_state(RazerVarstore variable_storage, RazerLedId led_id, RazerClassicLedState led_state);
 razer_report razer_chroma_standard_get_led_state(RazerVarstore variable_storage, RazerLedId led_id);
@@ -73,7 +75,7 @@ razer_report razer_chroma_standard_get_keyboard_layout();
 razer_report razer_chroma_standard_matrix_effect(RazerMatrixEffectId effect);
 razer_report razer_chroma_standard_matrix_set_custom_frame(uchar row_index, uchar start_col, uchar stop_col, const uchar *rgb_data);
 razer_report razer_chroma_extended_mouse_matrix_effect(RazerVarstore variable_storage, RazerLedId led_id, RazerMouseMatrixEffectId effect);
-razer_report razer_chroma_extended_matrix_set_brightness(RazerVarstore variable_storage, RazerLedId led_id, unsigned char brightness);
+razer_report razer_chroma_extended_matrix_set_brightness(RazerVarstore variable_storage, RazerLedId led_id, uchar brightness);
 razer_report razer_chroma_extended_matrix_get_brightness(RazerVarstore variable_storage, RazerLedId led_id);
 
 razer_report razer_chroma_misc_one_row_set_custom_frame(uchar start_col, uchar stop_col, const uchar *rgb_data);
